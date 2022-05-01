691267bfe0615f9c9bc7f9ac2e8faa75c33d8ff9
xtryfix <facyoucraft@gmail.com>
2022-05-01 16:44:35 +0300

    Подключение к бд сайта

--- eng/database.php
+++ eng/database.php
@@ -1,5 +1,5 @@
 <?php
-$bd = mysqli_connect('localhost', 'root', '', 'autoparts');
+$bd = mysqli_connect('localhost', 'idoma_autoparts', 'Root123', 'idoma_autoparts');
 if (!$bd) {
     die('Error connect to DB');
 }
